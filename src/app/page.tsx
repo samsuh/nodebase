1f60d1ab85981b5cdcb6773ac65333bac648616a
Sam Suh <samsuh@gmail.com>
2025-10-23 05:12:33 +0900

    add shadcn component libraries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
 import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
 export default function Home() {
-  return <div className={cn('text-red-500 font-bold')}>Hello NodeBase</div>
+  return (
+    <div className={cn('text-red-500 font-bold')}>
+      Hello NodeBase <Button>Clickable</Button>
+    </div>
+  )
 }
