0d1fb62e6c33fb4bad2ef19953b0085687803bc9
Samuel Suh <samsuh@gmail.com>
2025-11-04 17:36:33 +0900

    example for using trpc query from server

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,17 +1,10 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { z } from 'zod'
+import { baseProcedure, createTRPCRouter } from '../init'
+import prisma from '@/lib/db'
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      }),
-    )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }),
-});
+  getUsers: baseProcedure.query(() => {
+    return prisma.user.findMany()
+  }),
+})
 // export type definition of API
-export type AppRouter = typeof appRouter;
+export type AppRouter = typeof appRouter
